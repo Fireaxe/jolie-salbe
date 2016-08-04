e60b427683a670dd42482e156f6b50c5cd6f1cf2
Alex <alexandre.vaast@fdmanagement.fr>
2016-08-04 17:43:15 +0200

    debut bo + suite peche

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('AppBundle::index.html.twig', [
+        return $this->render('AppBundle:Front:index.html.twig', [
 
         ]);
     }
